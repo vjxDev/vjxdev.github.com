9f5e7d71175dd97c20e58f3f6fa9c8e3917e5b29
vjxDev <vjxdev@gmail.com>
2020-04-10 16:38:24 +0200

    prey

---
+++ random/prezentacija/js/main.js
@@ -0,0 +1,13 @@
+Reveal.initialize({
+  hash: true,
+  width: "100%",
+  height: "100%",
+  margin: 0,
+  minScale: 1,
+  maxScale: 1,
+  dependencies: [
+    { src: "plugin/markdown/marked.js" },
+    { src: "plugin/markdown/markdown.js" },
+  ],
+});
+
